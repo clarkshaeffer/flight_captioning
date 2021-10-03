949b06a29a49f3e56b2955e78c05edfe9a988f3f
Eric Waters <eswaters@asu.edu>
2021-10-03 09:15:22 -0700

    plz

--- js/index.js
+++ js/index.js
@@ -57,7 +57,7 @@ const run = async () => {
     }
   } else {
 
-    const response = await fetch('aa-inflight-captions.herokuapp.com'); // get temp session token from server.js (backend)
+    const response = await fetch('https://aa-inflight-captions.herokuapp.com/'); // get temp session token from server.js (backend)
     const data = await response.json();
     const { token } = data;
 
