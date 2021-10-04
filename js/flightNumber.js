7c07a6f0ae5cd0a4add807c4dbbc65a50b490290
Eric Waters <eswaters@asu.edu>
2021-10-04 13:53:27 -0700

    please

--- js/flightNumber.js
+++ js/flightNumber.js
@@ -1,6 +1,6 @@
 function validateForm() {
     const flightId = document.getElementById('flightInput').value;
     sessionStorage.setItem('flightNumber', flightId);
-    location.href = location.protocol + '//' + location.host + '/html/record/';
+    location.href = location.protocol + '//' + location.host + '/html/record';
     return false;
 }
